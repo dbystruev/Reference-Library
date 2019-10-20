6d12ac305c135e9c5b7c0e186f162836880af356
dbystruev <github@learnswift.ru>
2019-10-20 13:41:34 +0300

    Implemented term selection

--- Reference Library/ViewController.swift
+++ Reference Library/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var button: UIButton!
+    
+    // MARK: - Properties
+    let terms = ["Apple", "Banana", "Pear"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: - UIViewController Methods
+    override func viewDidAppear(_ animated: Bool) {
+        presentAlert()
     }
-
-
+    
+    // MARK: - Custom Methods
+    func presentAlert() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        terms.forEach { term in
+            let action = UIAlertAction(title: term, style: .default, handler: { action in
+                guard let term = action.title else { return }
+                self.button.setTitle(term, for: [])
+                self.button.isHidden = false
+                self.presentReference(for: term)
+            })
+            alert.addAction(action)
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    func presentReference(for term: String) {
+        if UIReferenceLibraryViewController.dictionaryHasDefinition(forTerm: term) {
+            let reference = UIReferenceLibraryViewController(term: term)
+            present(reference, animated: true)
+        }
+    }
+    
+    // MARK: - Actions
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        presentAlert()
+    }
 }
 
